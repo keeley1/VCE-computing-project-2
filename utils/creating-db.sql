fe52f5aa5404f9b31ebcbae4ffac16e04fe2e69f
Keeley Gardner <116581328+keeley1@users.noreply.github.com>
2023-11-17 21:20:21 +0000

    adding resources for readme

--- utils/creating-db.sql
+++ utils/creating-db.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS virtual_chat_extension;
 USE virtual_chat_extension;
 
-CREATE TABLE messages IF NOT EXISTS (id INT AUTO_INCREMENT, username VARCHAR(255) NOT NULL, message TEXT NOT NULL, message_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id));
+CREATE TABLE messages (id INT AUTO_INCREMENT, username VARCHAR(255) NOT NULL, message TEXT NOT NULL, message_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id));
 
 #create user for database
 CREATE USER 'test_user'@'localhost' IDENTIFIED BY 'password';
